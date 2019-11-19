5a0081d7765d8a2dbdcf85e40b115e2bd0b664ec
Dalvarado4 <57949328+Dalvarado4@users.noreply.github.com>
2019-11-19 07:45:44 -0600

    Creacion del Proyecto
    Proyecto Creado sin Ningun Problema

---
+++ Centro/Centro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Centro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
